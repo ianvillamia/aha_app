ba13e534b501360a9d49dc3eb28ea6bb49c152f4
ianvillamia <ianvillamia0123@gmail.com>
2020-05-13 00:46:48 +0800

    fcm ~working

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,8 @@ import 'package:aha_app/Services/Routing/routes.dart';
 import 'package:flutter/material.dart';
 // import 'package:aha_app/homepage.dart';
 import 'package:flutter/services.dart';
-
+import 'package:provider/provider.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
@@ -15,15 +16,42 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+    final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        //_showItemDialog(message);
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+        //_navigateToItemDetail(message);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      //  _navigateToItemDetail(message);
+      },
+    );
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/home',
-      onGenerateRoute: FluroRouter.router.generator,
-      home: Homepage(),
-    );
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/home',
+        onGenerateRoute: FluroRouter.router.generator,
+        home: Homepage(),
+      );
   }
+  
 }
